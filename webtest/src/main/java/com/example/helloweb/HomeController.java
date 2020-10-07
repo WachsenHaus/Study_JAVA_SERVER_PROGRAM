0c443fbb10eea1457c89ca09b9173eb8ce99c890
Younghoon CHOI _^ <wachsenhaus@gmail.com>
2020-10-07 12:42:21 +0900

    <추가> : <홈 연결 추가>

--- webtest/src/main/java/com/example/helloweb/HomeController.java
+++ webtest/src/main/java/com/example/helloweb/HomeController.java
@@ -5,8 +5,11 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+@Controller
 public class HomeController {
 	
 	//request mapping(value="/",method="requestMethod.get)
